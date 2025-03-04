7f4efc2531399d0b1b1f29702333729dcb7089f3
Vitor RD <corpmalfex@gmail.com>
2025-03-04 13:09:43 -0300

    Create parser.ps1

---
+++ parser.ps1
@@ -0,0 +1,14 @@
+$paths = Get-Content -Path .\paths.txt | ForEach-Object {
+    $parts = $_ -split "`t"
+    if ($parts.Count -ge 2) {
+        $parts[1]
+    }
+}
+
+# Remove linhas vazias e duplicatas
+$filteredPaths = $paths | Where-Object { $_ -ne "" } | Select-Object -Unique
+
+# Cria o arquivo de saída
+$filteredPaths | Out-File -FilePath .\filtered_paths.txt -Encoding UTF8
+
+Write-Host "$((Get-Item .).FullName)\pathszinho.txt"
